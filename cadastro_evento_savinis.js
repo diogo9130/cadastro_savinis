3e2c73f9a3e24f07f256c40456bf121e3e179c59
diogo silva <diogosilvastz@gmail.com>
2022-01-25 20:10:15 -0300

    Inputs adicionados no arquivo cadastro_evento_savinis.js

--- cadastro_evento_savinis.js
+++ cadastro_evento_savinis.js
@@ -1,29 +1,39 @@
 // Cadastro evento savinis
 
+import entradaDados from "readline-sync"
+
 let infoEvent = {
-    dataEvento: new Date ("2022-01-25"),
+    dataEvento: new Date ("2022-01-26"),
     dataAtual: new Date ()
 } 
 
-let infoPart = {
-    idade: 20,
-    quanPart: 70
-} 
+let quanPart = [
+    "Carlos",
+    "Rogério",
+    "Fernando",
+    "Ana",
+    "Miguel"
+]
 
 if (infoEvent.dataEvento > infoEvent.dataAtual) {
-    console.log("\nData válida.")
-    if (infoPart.idade > 18){
-        console.log("\nIdade permitida.")
-        if (infoPart.quanPart < 100) {
-            console.log("\nCadastro realizado.")
+    console.log("\nA data inserida no cadastro está disponível.");
+
+    let nome = entradaDados.question("\nInsira seu nome: ");
+    let idade = entradaDados.question("\nInsira a sua idade: ");
+
+    if (idade >= 18){
+        if (quanPart.length < 100) {
+            quanPart.push(nome);
+            console.log("\nCadastro realizado.");
         } else {
-            console.log("\nCadastro não realizado. Excedido o limite de participantes.")
+            console.log("\nCadastro não realizado. Excedido o limite de participantes.");
         }
     } else {
-        console.log("\nIdade não permitida.")
+        console.log("\nIdade não permitida.");
     }
 } else {
-    console.log("\nData inválida.")
+    console.log("\nA data inserida no cadastro não está disponível.");
 }
-console.log("\nObrigado por utilizar noso cadastro")
+console.log("\nObrigado por utilizar noso cadastro.");
+
 
